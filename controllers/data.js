c3b28fd09fe4cbd210a819e10d8733d25d079628
Muhammad Umer <mufkamran.dikhan.pk@gmail.com>
2024-06-28 06:39:31 +0500

    bug fixed

--- controllers/data.js
+++ controllers/data.js
@@ -60,10 +60,7 @@ const getCommonData = async (req, res) => {
   try {
     const { q } = req.query;
     const allData = await dataModel
-      .find()
-      .select(
-        "query.google_query.url query.google_news_query.url query.querySearch -_id -location -date -language -plugin_id -query.google_query.heading -query.google_news_query.heading -query.google_news_query.time_stamp"
-      );
+      .find({},"query.google_query.url query.google_news_query.url query.querySearch -_id -location -date -language -plugin_id -query.google_query.heading -query.google_news_query.heading -query.google_news_query.time_stamp")
     let google_query_url = new Set();
     let google_query_news_url = new Set();
     let google_url_count = [];
